f8b9e5cdfd38504d6013a9bf76c60a9e4b90f387
Objelisks <supertyrian@gmail.com>
2014-07-14 19:39:49 -0700

    removed dumb square

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ website
 =======
 
 Personal Website
+
+
+Home
+-
+Empty landing page
+
+Projects
+-
+List of projects generated from config file.
+Images, description, title, links.
+
+Stream
+-
+Simple blog posts. Formats images, video, text.
+each post separated by minimal spacing.
+layout options: stream, tag cloud, source
+tags
+
+Contact
+-
+Links
+
+Hidden Level
+-
+Experimental projects
+
+
+Structure
+=========
+
+Entirely written in javascript using d3 to add elements.
+On page load
+build home page
+Links go to fragments and call some build function.
+Destroys previous page while saving any changed data to local storage
+Save loaded page to local storage to load back faster later.
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var linksData = [
 	{
 		name: "Home",
-		url: "#home"
+		url: "#home",
+		build: buildHome
 	},
 	{
 		name: "Projects",
@@ -22,6 +23,8 @@ var linkStyle = {
 };
 
 var body = d3.select(document.body);
+var currentPage = "Home";
+sessionStorage['pages'] = sessionStorage['pages'] || {};
 
 function buildHeader() {
 	var header = body.append('section');
@@ -29,77 +32,26 @@ function buildHeader() {
 	links.enter().append('a')
 		.text(function(d) { return d.name; })
 		.attr('href', function(d) { return d.url; })
-		.style(linkStyle);
+		.style(linkStyle)
+		.on('click', function(d) {
+		  // Get the content of the page as a dom element
+		  var content;
+		  if(sessionStorage['pages'][d.name]) {
+		    // If the page has been loaded previously, we can just get it from store
+		    content = sessionStorage['pages'][d.name];
+		  } else {
+		    // Otherwise, build the page fresh.
+		    content = d.build();
+		  }
+
+		  // Display the content
+		});
+}
+
+function buildCurrentPage() {
+  // Get the fragment
+  // Build the page
 }
 
 buildHeader();
-
-body.append('div')
-  .attr('id', 'box')
-  .style('width', '100px')
-  .style('height', '100px')
-  .style('top', '100px')
-  .style('left', '100px')
-  .style('position', 'absolute')
-  .style('background-color', 'steelblue');
-
-var boxLeft = 100;
-var boxTop = 100;
-body.on('click', function() {
-  console.log('turning');
-  d3.selectAll('#box')
-    .transition().duration(250)
-    .style('transform', 'rotate(0.25turn)')
-    .style('left', function(d) { return boxLeft + 100 + 'px'; });
-  boxLeft += 100;
-});
-
-d3.select(window).on('mousedown', function() {
-  console.log('mousemove');
-  var mouseX = d3.event.clientX;
-  var mouseY = d3.event.clientY;
-  
-  var box = d3.selectAll('#box');
-  
-  var diffX = parseFloat(box.style('left')) - mouseX;
-  var diffY = parseFloat(box.style('top')) - mouseY;
-  var move = function(dir, n) { return function() {
-    var box = d3.select(this);
-    var pos = parseFloat(box.style(dir));
-    box.style(dir, (pos + n) + 'px');
-    console.log(dir + ' ' + box.style(dir) + ' ' + pos + ' ' + n);
-    box.style('transform', '');
-    box.interrupt();
-  }};
-  
-  var moveEnd;
-  
-  if(Math.abs(diffX) > Math.abs(diffY)) {
-    if(diffX > 100) {
-      // go left
-      console.log('left');
-      box = box.style('transform-origin', '0% 0%');
-      moveEnd = move('left', -100);
-    } else if(diffX < -100) {
-      // go right
-      console.log('right');
-      box = box.style('transform-origin', '100% 100%');
-      moveEnd = move('left', 100);
-    }
-  } else { 
-    if(diffY > 100) {
-      // go up
-      console.log('up');
-      box = box.style('transform-origin', '100% 0%');
-      moveEnd = move('top', -100);
-    } else if(diffY < -100) {
-      // go down
-      console.log('down');
-      box = box.style('transform-origin', '0% 100%');
-      moveEnd = move('top', 100);
-    }
-  }
-  box.transition().duration(1000)
-    .style('transform', 'rotate(0.25turn)')
-    .each('end', moveEnd);
-});
+buildCurrentPage();
