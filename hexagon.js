6359d6f396eafbd5958e7f0062bb4c73dae57b72
Rather <supertyrian@gmail.com>
2015-01-06 22:17:52 -0800

    pages

--- hexagon.js
+++ hexagon.js
@@ -1,135 +1,185 @@
-// center design
+var Tween = createjs.Tween;
+var Ease = createjs.Ease;
+paper.install(window);
 
-// symbols
-//  home
-//  projects
-//  experiment
-//  contact
-
-
-// website
-//  links: request json from server api
-
-// architecture
-//  server:
-//   get('key') -> {json}
-//   send('key', {value}) -> newValue
-
-// get('projects')
-// get('contactinfo')
-
-var colors = [
-  0x3F6C8F, // light blue
-  0x243542, // dark blue
-  0x4F3332, // dark red
-  0x747533 // dark yellow
-];
-
-var lsystem = "CCCC";
-var rules = {
-  "F": "FFCFFFCCCF",
-  "C": "C-FFF-CCFCCCFFFFF-F-CF+CFFC[F+-F-FF]F"
+var colors = {
+  background: "#4B6385",
+  text: "#7EA8E0",
+  color1: "#B5775D",
+  color2: "#AE62BD",
+  color3: "#509C67",
+  color4: "#918D4A"
 };
 
-var replace = function(str, rules) {
-  var out = '', repl;
-  str.split('').forEach(function(ch) {
-    repl = rules[ch];
-    if(repl) {
-      out += repl;
-    } else {
-      out += ch;
+var pages = {};
+
+var persistentPageData = {
+  links: [
+  ]
+};
+
+// note to self: redo lsystembot to look more like this
+function generateLSystemPath(lsystem, rules, angle, iter, color) {
+  var replace = function(str, rules) {
+    var out = '', repl;
+    str.split('').forEach(function(ch) {
+      repl = rules[ch];
+      if(repl) {
+        out += repl;
+      } else {
+        out += ch;
+      }
+    });
+    return out;
+  };
+
+  var next = lsystem;
+  for(var i=0; i<iter; i++) {
+    next = replace(next, rules);
+  }
+
+  var designsegments = [];
+  var cmds = next.split('');
+  var currentPoint = new Point(0, 0);
+  var movement = new Point(0, -10);
+  var stack = [];
+  var translations = {
+    'F': function(path) { currentPoint = currentPoint.add([movement.x, movement.y]); path.lineTo(currentPoint); },
+    '+': function(path) { movement = movement.rotate(angle, new Point(0, 0)); },
+    '-': function(path) { movement = movement.rotate(-angle, new Point(0, 0)); },
+    '[': function(path) { stack.push(currentPoint); stack.push(movement); },
+    ']': function(path) { movement = stack.pop(); currentPoint = stack.pop(); path.moveTo(currentPoint); }
+  };
+
+  var path = new CompoundPath();
+  path.moveTo(currentPoint);
+  cmds.forEach(function(cmd) {
+    var translation = translations[cmd];
+    if(translation) {
+      translation(path);
     }
   });
-  return out;
-};
 
-var next = lsystem;
-next = replace(next, rules);
-next = replace(next, rules);
-next = replace(next, rules);
-console.log(next);
+  path.strokeColor = color;
+  path.strokeWidth = 1;
+  path.fitBounds(new Rectangle(-600, -600, 1200, 1200));
+  path.smooth();
+  return path;
+}
 
-var designsegments = [];
-var cmds = next.split('');
-var currentPoint = new Point(0, 0);
-var movement = new Point(0, -10);
-var angle = 90;
-var stack = [];
-var translations = {
-  'F': function(path) { currentPoint += movement; path.lineTo(currentPoint); },
-  '+': function(path) { movement = movement.rotate(angle, new Point(0, 0)); },
-  '-': function(path) { movement = movement.rotate(-angle, new Point(0, 0)); },
-  '[': function(path) { stack.push(currentPoint); stack.push(movement); },
-  ']': function(path) { movement = stack.pop(); currentPoint = stack.pop(); path.moveTo(currentPoint); }
-};
+function makeLink(text, pos) {
+  var link = new PointText();
+  link.position.x = pos.x;
+  link.position.y = pos.y;
+  link.content = text;
+  link.fontFamily = "open-sans-condensed";
+  link.fontSize = "32pt";
+  link.fillColor = colors.text;
+  link.justification = "center";
+  link.onClick = function() {
+    switchTo(pages[text]);
+  };
+  return link;
+}
 
-var path = new CompoundPath();
-path.moveTo(currentPoint);
-cmds.forEach(function(cmd) {
-  var translation = translations[cmd];
-  if(translation) {
-    translation(path);
+function makeLayer(path, linkLocations) {
+  var layer = new Layer();
+  layer.addChild(path);
+  layer.data.linkLocations = linkLocations;
+  layer.visible = false;
+  layer.position = new Point(0, 0);
+  layer.applyMatrix = false;
+  return layer;
+}
+
+function switchTo(page) {
+  Object.keys(pages).forEach(function(pageKey) {
+    pages[pageKey].visible = false;
+  });
+  page.visible = true;
+  persistentPageData.links.forEach(function(link, i) {
+    var targetPos = page.data.linkLocations[i];
+    link.position = targetPos;
+    //Tween.get(link.position, {override:true}).to({x: targetPos.x, y: targetPos.y}, 300.0, Ease.cubicInOut);
+  });
+  page.addChildren(persistentPageData.links);
+  if(project.activeLayer !== page) {
+    Tween.get(project.activeLayer).to({opacity: 0.0}, 300.0);
+    page.opacity = 0.0;
+    Tween.get(page).to({opacity: 1.0}, 300.0);
   }
-});
-
-path.strokeColor = '#4F3332';
-path.strokeWidth = 2;
-path.fitBounds(new Rectangle(-600, -600, 1200, 1200));
-path.smooth();
-//path.translate(new Point(-path.bounds.width/4, -path.bounds.height/4));
-
-
-var home = new PointText(new Point(-200, -200));
-home.content = "home";
-home.fontFamily = "open-sans-condensed";
-home.fontSize = "32pt";
-home.fillColor = "#3F6C8F";
-home.justification = "center";
-
-var projects = new PointText(new Point(200, -200));
-projects.content = "projects";
-projects.fontFamily = "open-sans-condensed";
-projects.fontSize = "32pt";
-projects.fillColor = "#3F6C8F";
-projects.justification = "center";
-
-var unknown = new PointText(new Point(-200, 200));
-unknown.content = "unknown";
-unknown.fontFamily = "open-sans-condensed";
-unknown.fontSize = "32pt";
-unknown.fillColor = "#3F6C8F";
-unknown.justification = "center";
-
-var contact = new PointText(new Point(200, 200));
-contact.content = "contact";
-contact.fontFamily = "open-sans-condensed";
-contact.fontSize = "32pt";
-contact.fillColor = "#3F6C8F";
-contact.justification = "center";
-
-var hex = new Path.RegularPolygon(new Point(0, 0), 6, 50);
-//hex.strokeColor = "black";
-//hex.fillColor = "black";
-hex.rotate(90);
-hex.strokeJoin = 'round';
-hex.strokeWidth = 15;
-
-var scale = 1.0;
-var scaleAnimScale = 10.0;
-var tick = 0;
-
-function onFrame(event) {
-    tick += 0.01;
-    scale = 1.0 + Math.sin(tick * 2.0) / 400.0;
-
-    hex.scale(scale);
+  page.activate();
+  page.position.x = view.bounds.center.x;
+  page.position.y = view.bounds.center.y;
 }
 
-function onResize() {
-  project.activeLayer.position = view.center;
+function init() {
+  persistentPageData.links.push(makeLink("home", new Point(-200, -140)));
+  persistentPageData.links.push(makeLink("projects", new Point(200, -160)));
+  persistentPageData.links.push(makeLink("unknown", new Point(-200, 180)));
+  persistentPageData.links.push(makeLink("contact", new Point(200, 160)));
+
+  pages.home = makeLayer(generateLSystemPath("CCCC", {
+    "F": "FFCFFFCCCF",
+    "C": "C-FFF-CCFCCCFFFFF-F-CF+CFFC[F+-F-FF]F"
+  }, 90, 3, colors.color1), [
+    new Point(-200, -150),
+    new Point(200, -170),
+    new Point(-200, 170),
+    new Point(200, 150)
+  ]);
+
+  pages.projects = makeLayer(generateLSystemPath("FFF", {
+      "F": "F-F-F--F-F+FF[+-F+FF-FFF]F+FY++",
+      "Y": "-"
+  }, 90, 3, colors.color2), [
+    new Point(-340, -230),
+    new Point(-470, -150),
+    new Point(-415, -70),
+    new Point(-560, 30)
+  ]);
+
+  pages.unknown = makeLayer(generateLSystemPath("FF", {
+      "F": "UUUFUFF-F[UFF+F-F-+UUF+FUFF[--]+UU]FF",
+      "U": "FFFFF+FUU"
+  }, 45, 3, colors.color3), [
+    new Point(280, -230),
+    new Point(200, -150),
+    new Point(260, -30),
+    new Point(340, 40)
+  ]);
+
+  pages.contact = makeLayer(generateLSystemPath("FFAFAF", {
+      "F": "FFF+A+FA-F+++FFFF-F-AFAAF+-+",
+      "A": "AFFFAF+FFFFAF+-FFA"
+  }, 60, 3, colors.color4), [
+    new Point(200, -230),
+    new Point(420, -230),
+    new Point(420, -140),
+    new Point(420, -50)
+  ]);
+
+  view.onFrame = function(event) {
+    //Tween.tick(event.delta);
+  }
+  view.onResize = function(event) {
+    project.activeLayer.position.x = view.bounds.center.x;
+    project.activeLayer.position.y = view.bounds.center.y;
+  }
 }
 
-function grow() {
+function setupCanvas() {
+  paper.setup('canvas');
+  document.getElementById('canvas').style.background = colors.background;
+  document.getElementById('canvas').style.width = '100%';
+  document.getElementById('canvas').style.height = '100%';
+}
 
+
+window.onload = function() {
+  createjs.Ticker.framerate = 60;
+  setupCanvas();
+  init();
+  switchTo(pages.home);
+  view.draw();
 }
